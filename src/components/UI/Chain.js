70279a6a9bfd57879317790e26012cc641c2ed2b
Kirill Hannitskyi <lerand94@gmail.com>
2021-09-16 15:33:34 +0300

    dsdsds

--- src/components/UI/Chain.js
+++ src/components/UI/Chain.js
@@ -1,6 +1,13 @@
 import chain from "../../assets/chain.png";
+import chainOpt from "../../assets/chain.webp";
 import classes from "./Chain.module.css";
 const Chain = () => {
-  return <img src={chain} className={classes.chain} />;
+  return (
+    <picture>
+      <source srcSet={chainOpt} type="image/webp" />
+      <source srcSet={chain} type="image/png" />
+      <img src={chain} alt="chain" className={classes.chain} />
+    </picture>
+  );
 };
 export default Chain;
